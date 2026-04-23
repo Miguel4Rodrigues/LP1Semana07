07cb96a0b08659016f7e438dda94b2a9e35ec187
Miguel Rodrigues <miguel4rodrigues@gmail.com>
2026-04-23 16:35:57 +0100

    Update class Cell

--- PowerCell/Cell.cs
+++ PowerCell/Cell.cs
@@ -8,10 +8,31 @@ namespace PowerCell
         private float charge;
         private string Name { get; }
         private float Charge;
-        private readonly int Level;
+        private int Level
+        {
+            get => 1 + (int)Charge / 40;
+        }
+
+        public Cell(string name)
+        {
+            Name = name;
+            Charge = 200;
+        }
+
+        public void Consume(float amount)
+        {
+            Charge -= amount;
+        }
+
+        public void Restore()
+        {
+            Charge = 200;
+        }
+
+        
         public override string ToString()
         {
-            return $"[Name] Level {Level}: {Charge}/200";
+            return $"[{Name}] Level {Level}: {Charge}/200";
         }
     }
 }
